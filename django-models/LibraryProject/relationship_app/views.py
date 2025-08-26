a135ee91eb1d062c5ecd104b7959da3c443e7c33
bestheart-ups <shaibuaugustine507@gmail.com>
2025-08-26 14:16:52 +0100

    worked on views and urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Bookshelf
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
+
+
+from django.views.generic.detail import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Library has a related_name 'books' for its Book relationship
+        context['books'] = self.object.books.all()
+        return context
